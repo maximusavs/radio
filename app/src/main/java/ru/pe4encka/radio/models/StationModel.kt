cf4956b470b2d49b3f59f6726a83da838d166bb4
mick3247652 <mick.dunaev@yandex.ru>
2019-05-26 08:16:27 +0300

    initial commit'

---
+++ app/src/main/java/ru/pe4encka/radio/models/StationModel.kt
@@ -0,0 +1,23 @@
+package ru.pe4encka.radio.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.serialization.Optional
+import kotlinx.serialization.Serializable
+
+@Serializable
+@Entity(tableName = "stations")
+data class StationModel(
+    @PrimaryKey val id: Int,
+    val name:String,
+    val desc:String,
+    val icon:String,
+    val slogan:String,
+    val tags: String,
+    val stream:String,
+    val logo:String,
+    val language:String,
+    val format: String,
+    val locate: String,
+    @Optional var recent: Boolean = false
+)
